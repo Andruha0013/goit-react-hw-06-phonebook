7c94f6782970a4e2c3e8e5db5798c56abbe5d00a
Andruha0013 <andruha0013@gmail.com>
2023-04-08 11:05:36 +0300

    fix worning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 //import React, { Component } from "react";
-import React,{useEffect, useState} from "react";
+import React from "react";
 //import { nanoid } from 'nanoid';
 //import LocalStorApp from "./myLib/LocalStorApp";
 
