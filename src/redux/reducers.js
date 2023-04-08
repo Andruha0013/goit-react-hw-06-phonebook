09257170a9331605238ff483dd703833c75d248d
Andruha0013 <andruha0013@gmail.com>
2023-04-08 11:20:14 +0300

    break fix

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -28,7 +28,7 @@ function checkEqualStr(val1,val2){
     return state;
 };*/
 //----------------------contacts-----------------------------------------------
-export const contactsReducer=(state = initConstactsState, action)=>{
+/*export const contactsReducer=(state = initConstactsState, action)=>{
     switch(action.type){
         //----------------add-contact------------------------------------------
         case "contacts/addContact": 
@@ -54,12 +54,44 @@ export const contactsReducer=(state = initConstactsState, action)=>{
         //====================================================================
         default: myStorage.setData(state); return state;
     }
+};*/
+
+export const contactsReducer=(state = initConstactsState, action)=>{
+        //----------------add-contact------------------------------------------
+        if(action.type==="contacts/addContact"){
+        //console.log(state);
+            if(state.findIndex((item)=>{
+                if(checkEqualStr(action.payload.name,item.name)){
+                    //console.log(`object with name=${item.name} is already exists`);
+                    alert(`object with name=${item.name} is already exists`)
+                    return true;
+                }
+                if(checkEqualStr(action.payload.number,item.number)){
+                    //console.log(`object with number=${item.number} is already exists`);
+                    alert(`object with number=${item.number} is already exists`);
+                    return true;
+                }
+                return false;
+            })<0){
+                myStorage.setData(state); 
+                return [...state,action.payload];
+            }
+        }
+        //-----------------------del-contact-----------------------------------
+        if(action.type==="contacts/delContact"){
+            myStorage.setData(state); 
+            return state.filter(contact=>contact.id!==action.payload);
+        }
+            
+        //====================================================================
+        return state;
+    
 };
 //------------------------filter-----------------------------------------------
 export const filterReducer=(state=initFilterlState,action)=>{
     switch(action.type){
         case "filter/setFilter":
-            return action.payload;
+            return action.payload;break;
         default: return state;
     }
 }
