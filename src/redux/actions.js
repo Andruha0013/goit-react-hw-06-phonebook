c8509ffb0db9b75cdd76158be285e25a8e54a2a5
Andruha0013 <andruha0013@gmail.com>
2023-04-09 19:01:33 +0300

    prtier2

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,27 +1,26 @@
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
 
-
-export const addContact=contact=>{
-    return {
-        type:"contacts/addContact",
-        payload:{
-            id:nanoid(),
-            name:   contact.name,
-            number: contact.number,
-        },
-    };
-}
-
-export const delContact=id=>{
-    return {
-        type:   "contacts/delContact",
-        payload: id,
-    };
-}
-//------------------------------------------
-export const setFilter=str=>{
+export const addContact = contact => {
   return {
-        type: "filter/setFilter",
-        payload: str,
-  }
-}
+    type: 'contacts/addContact',
+    payload: {
+      id: nanoid(),
+      name: contact.name,
+      number: contact.number,
+    },
+  };
+};
+
+export const delContact = id => {
+  return {
+    type: 'contacts/delContact',
+    payload: id,
+  };
+};
+//------------------------------------------
+export const setFilter = str => {
+  return {
+    type: 'filter/setFilter',
+    payload: str,
+  };
+};
