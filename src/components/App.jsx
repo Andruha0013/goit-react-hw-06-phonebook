b940e318f784c0e690a0f12181db0f18f74e5288
Andruha0013 <andruha0013@gmail.com>
2023-04-09 19:00:56 +0300

    pritier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,19 @@
+import React from 'react';
 
-import React from "react";
+import ContactForm from './Phonebook/ContactForm/ContactForm';
+import Filter from './Phonebook/Filter/Filter';
+import ContactList from './Phonebook/ContactList/ContactList';
+import css from './Phonebook.module.css';
 
-import ContactForm from "./Phonebook/ContactForm/ContactForm";
-import Filter from "./Phonebook/Filter/Filter";
-import ContactList from "./Phonebook/ContactList/ContactList";
-import css from "./Phonebook.module.css"
-
-export default function App(){
-
-//-----------------------------------------------------------------------------------
+export default function App() {
+  //-----------------------------------------------------------------------------------
   return (
     <div className={css.phoneBook}>
       <h1>Phonebook</h1>
-      <ContactForm
-      />
+      <ContactForm />
       <h2>Contacts</h2>
-      <Filter/>
-      <ContactList 
-      />
+      <Filter />
+      <ContactList />
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer, filterReducer } from "./reducers";
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer, filterReducer } from './reducers';
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer,
-    },
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
